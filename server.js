1435584b7465648ed72ab30d589aaa1299f39964
Tibegex <joshua_b_lee@yahoo.com>
2021-06-21 19:08:20 -0700

    updated connection

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/vast-brushlands-59087",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 
